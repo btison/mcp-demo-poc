e1494f02195bd30be69e35ebb4c0d97ddce6d0c2
Bernard Tison <bernard.tison@gmail.com>
2026-03-12 10:22:49 +0100

    Programmatic lookup of agent based on qualifier

--- apps/globex-agent/src/main/java/org/globex/ai/agent/AgentManager.java
+++ apps/globex-agent/src/main/java/org/globex/ai/agent/AgentManager.java
@@ -1,19 +1,27 @@
 package org.globex.ai.agent;
 
+import io.quarkus.arc.Arc;
+import io.quarkus.arc.ArcContainer;
+import io.quarkus.arc.InjectableInstance;
+import io.quarkus.logging.Log;
 import io.smallrye.common.annotation.Identifier;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
+import jakarta.enterprise.inject.UnsatisfiedResolutionException;
 
 @ApplicationScoped
 public class AgentManager {
 
-    @Inject @Identifier("routing-agent")
-    Agent routingAgent;
-
     public static final String ROUTING_AGENT = "routing-agent";
 
     public Agent getAgent(String agentName) {
-        return routingAgent;
+        ArcContainer container = Arc.container();
+        InjectableInstance<Agent> agentInstanceHandle = container.select(Agent.class, Identifier.Literal.of(agentName));
+        if (agentInstanceHandle != null && agentInstanceHandle.isResolvable()) {
+            return agentInstanceHandle.get();
+        } else {
+            Log.warnf("Agent with qualifier %s not found", agentName);
+            throw new UnsatisfiedResolutionException("Agent with qualifier " + agentName + " not found");
+        }
     }
 
 }
